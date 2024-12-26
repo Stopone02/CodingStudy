d11b7f4a7106d16f39c17129bf3ad6406a81f5d7
정지원 <jjo0522@catholic.ac.kr>
2024-12-27 03:32:04 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,6 +1,6 @@
 def solution(str1, str2):
     answer = 2
-    for i in range(0, len(str1)):
+    for i in range(0, len(str1)-len(str2)+1):
         if str1[i:i+len(str2)] == str2:
             answer = 1
             break
