b03d6c85f5c1ef9f2b7bb8a4e81cc498b17a1179
정지원 <jjo0522@catholic.ac.kr>
2025-01-25 00:17:13 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    even = 0
+    odd = 0
+    for i in range(0, len(num_list)):
+        if i%2 == 0:
+            even += num_list[i]
+        else:
+            odd += num_list[i]
+    if even >= odd:
+        answer = even
+    else:
+        answer = odd
+    return answer
